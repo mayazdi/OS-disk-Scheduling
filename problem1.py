8000aad266b9b715d61dbe345a79908c728abc5f
Amin <yazdi.mohammad99@gmail.com>
2020-01-20 12:28:56 +0330

    Init Project

---
+++ problem1.py
@@ -0,0 +1,20 @@
+# Simulating OS's disk scheduling algorithms. Containing:
+# FCFS, SSTF, Scan, C-Scan, Look, C-Look
+
+def fcfs():
+    pass
+
+def sstf():
+    pass
+
+def scan():
+    pass
+
+def cscan():
+    pass
+
+def look():
+    pass
+
+def clook():
+    pass
