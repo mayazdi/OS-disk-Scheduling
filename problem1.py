affcff72dadf372b142155bfe96617db55edde7f
Amin <yazdi.mohammad99@gmail.com>
2020-01-20 15:55:02 +0330

    scan added still unstable

--- problem1.py
+++ problem1.py
@@ -15,6 +15,8 @@ def fcfs():
 def sstf():
     requests.append(current)
     requests.sort()
+    index = requests.index(current)
+    # turn if to while and check boundaries (end and begin)
     if len(requests) > index > 0:
         upper = abs(requests[index+1] - requests[index])
         lower = abs(requests[index-1] - requests[index])
@@ -27,11 +29,23 @@ def sstf():
             current += requests[index+1]
 
         requests.pop(index=index)
-        
 
 
 def scan():
-    pass
+    requests.append(current)
+    requests.sort()
+    index = requests.index(current)
+    # check if it is not at the end (both right and left)
+    #turn if to while
+    if right_direction:
+        add_distance(requests[index+1])
+        requests.pop(index=index)
+        index += 1
+    #indent into , check boundaries
+    if index == len(requests) - 1:
+        right_direction = False
+        add_distance(requests[index-1])
+
 
 
 def cscan():
@@ -51,6 +65,7 @@ f = open("requests.txt", "r")
 for line in f:
         requests.append(int(line))
 sum = 0
+right_direction = True
 a = input("Insert a number between 1 - 6:")
 current = input("Enter current sector:")
 
