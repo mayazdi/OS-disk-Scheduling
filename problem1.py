9e64b7adcd74a9752867ab33be55c5ec21f22fc0
Amin <yazdi.mohammad99@gmail.com>
2020-01-21 15:30:22 +0330

    Scan stable

--- problem1.py
+++ problem1.py
@@ -2,17 +2,21 @@
 # FCFS, SSTF, Scan, C-Scan, Look, C-Look
 
 
-def add_distance(destination):
-    sum += abs(current-destination)
-    return destination
+def add_distance(origin, destination):
+    return abs(origin-destination)
+    
 
 
-def fcfs():
-    for i in requests:
-        current = add_distance(i)
+def fcfs(current):
+    sum = 0
+    for r in requests:
+        sum += add_distance(current, r)
+        current = r
+    return sum
 
 
-def sstf():
+def sstf(current):
+    sum = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
@@ -23,29 +27,38 @@ def sstf():
         if upper > lower:
             sum += lower
             current += requests[index-1]
-            requests.pop(index=index)
+            requests.pop(index)
         else:
             sum += upper
             current += requests[index+1]
 
-        requests.pop(index=index)
+        requests.pop(index)
+    return sum
 
 
-def scan():
+def scan(direction):
+    sum = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
     # check if it is not at the end (both right and left)
-    #turn if to while
-    if right_direction:
-        add_distance(requests[index+1])
-        requests.pop(index=index)
-        index += 1
-    #indent into , check boundaries
-    if index == len(requests) - 1:
-        right_direction = False
-        add_distance(requests[index-1])
-
+    #Headed to Right
+    while len(requests) > 1:
+        if direction:
+            if index == len(requests) - 1:
+                direction = 0
+                continue
+            sum += add_distance(requests[index], requests[index+1])
+            requests.pop(index)
+        #Headed to Left
+        else:
+            if index == 0:
+                direction = 1
+                continue
+            sum += add_distance(requests[index-1], requests[index])
+            requests.pop(index)
+            index -= 1
+    return sum
 
 
 def cscan():
@@ -64,22 +77,21 @@ requests = []
 f = open("requests.txt", "r")
 for line in f:
         requests.append(int(line))
-sum = 0
+
 right_direction = True
-a = input("Insert a number between 1 - 6:")
-current = input("Enter current sector:")
-
-if a == '1':
-    fcfs()
-elif a == '2':
-    sstf()
-elif a == '3':
-    scan()
-elif a == '4':
+a = int(input("Insert a number between 1 - 6:"))
+current = int(input("Enter current sector:"))
+if a == 1:
+    fcfs(current)
+elif a == 2:
+    sstf(current)
+elif a == 3:
+    scan(right_direction)
+elif a == 4:
     cscan()
-elif a == '5':
+elif a == 5:
     look()
-elif a == '6':
+elif a == 6:
     clook()
-
 f.close()
+print(sum)
