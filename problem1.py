2fdb080f735dd78e5622b1a998dd143026f33dff
Amin <yazdi.mohammad99@gmail.com>
2020-01-24 18:13:57 +0330

    SSTF and minor fixes

--- problem1.py
+++ problem1.py
@@ -7,35 +7,44 @@ def add_distance(origin, destination):
 
 
 def fcfs(current):
-    sum = 0
+    total = 0
     for r in requests:
-        sum += add_distance(current, r)
+        total += add_distance(current, r)
         current = r
-    return sum
+    return total
 
 
 def sstf(current):
-    sum = 0
+    total = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
     # turn if to while and check boundaries (end and begin)
-    if len(requests) > index > 0:
-        upper = abs(requests[index+1] - requests[index])
-        lower = abs(requests[index-1] - requests[index])
-        if upper > lower:
-            sum += lower
-            current += requests[index-1]
-            requests.pop(index)
+    while len(requests) > 1:   
+        #fetch distances
+        if index != len(requests) - 1:
+            upper = abs(requests[index+1] - requests[index])
         else:
-            sum += upper
-            current += requests[index+1]
-        requests.pop(index)
-    return sum
+            upper = maximum + 1
+        if index != 0:
+            lower = abs(requests[index-1] - requests[index])
+        else:
+            lower = maximum + 1
+        #move head
+        if upper > lower:
+            total += lower
+            current = requests[index-1]
+            index -= 1
+            requests.pop(index + 1)
+        else:
+            total += upper
+            current = requests[index+1]
+            requests.pop(index)
+    return total
 
 
 def scan(direction):
-    sum = 0
+    total = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
@@ -45,21 +54,21 @@ def scan(direction):
             if index == len(requests) - 1:
                 direction = 0
                 continue
-            sum += add_distance(requests[index], requests[index+1])
+            total += add_distance(requests[index], requests[index+1])
             requests.pop(index)
         #Headed to Left
         else:
             if index == 0:
                 direction = 1
                 continue
-            sum += add_distance(requests[index-1], requests[index])
+            total += add_distance(requests[index-1], requests[index])
             requests.pop(index)
             index -= 1
-    return sum
+    return total
 
 
 def cscan(direction):
-    sum = 0
+    total = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
@@ -67,29 +76,29 @@ def cscan(direction):
     while len(requests) > 1:
         if direction:
             if index == len(requests) - 1:
-                sum += add_distance(requests[index], maximum)
+                total += add_distance(requests[index], maximum)
                 index = 0
                 requests.pop(len(requests)-1)
-                sum += add_distance(minimum, requests[index])
+                total += add_distance(minimum, requests[index])
                 continue
-            sum += add_distance(requests[index], requests[index+1])
+            total += add_distance(requests[index], requests[index+1])
             requests.pop(index)
         #Headed to Left
         else:
             if index == 0:
-                sum += add_distance(requests[index], minimum)
+                total += add_distance(requests[index], minimum)
                 requests.pop(0)
                 index = len(requests) - 1
-                sum += add_distance(maximum, requests[index])
+                total += add_distance(maximum, requests[index])
                 continue
-            sum += add_distance(requests[index-1], requests[index])
+            total += add_distance(requests[index-1], requests[index])
             requests.pop(index)
             index -= 1
-    return sum
+    return total
 
 
 def look(direction):
-    sum = 0
+    total = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
@@ -99,21 +108,21 @@ def look(direction):
             if index == len(requests) - 1:
                 index = 0
                 continue
-            sum += add_distance(requests[index], requests[index+1])
+            total += add_distance(requests[index], requests[index+1])
             requests.pop(index)
         #Headed to Left
         else:
             if index == 0:
                 index = len(requests) - 1
                 continue
-            sum += add_distance(requests[index-1], requests[index])
+            total += add_distance(requests[index-1], requests[index])
             requests.pop(index)
             index -= 1
-    return sum
+    return total
 
 
 def clook(direction):
-    sum = 0
+    total = 0
     requests.append(current)
     requests.sort()
     index = requests.index(current)
@@ -123,40 +132,41 @@ def clook(direction):
             if index == len(requests) - 1:
                 index = 0
                 continue
-            sum += add_distance(requests[index], requests[index+1])
+            total += add_distance(requests[index], requests[index+1])
             requests.pop(index)
         #Headed to Left
         else:
             if index == 0:
                 index = requests[len(requests)-1]
                 continue
-            sum += add_distance(requests[index-1], requests[index])
+            total += add_distance(requests[index-1], requests[index])
             requests.pop(index)
             index -= 1
-    return sum
+    return total
 
 
+movement = 0
 requests = []
 f = open("requests.txt", "r")
 for line in f:
         requests.append(int(line))
 
-right_direction = True
-maximum = 4999
+right_direction = False
 minimum = 0
+maximum = 4999
 a = int(input("Insert a number between 1 - 6:"))
 current = int(input("Enter current sector:"))
 if a == 1:
-    fcfs(current)
+    movement = fcfs(current)
 elif a == 2:
-    sstf(current)
+    movement = sstf(current)
 elif a == 3:
-    scan(right_direction)
+    movement = scan(right_direction)
 elif a == 4:
-    cscan(right_direction)
+    movement = cscan(right_direction)
 elif a == 5:
-    look(right_direction)
+    movement = look(right_direction)
 elif a == 6:
-    clook(right_direction)
+    movement = clook(right_direction)
 f.close()
-print(sum)
+print(movement)
